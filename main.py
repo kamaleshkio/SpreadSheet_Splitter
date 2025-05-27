8691dc5080c4148461dec8efd643ac2ed4f9ca0c
kamalesh <kamaleshbaskaran4310@gmail.com>
2025-05-27 15:33:24 +0530

    main update

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+from gui.file_browser import Choose_file, Choose_output_folder
+from modules.file_handler import process_excel_file
+from modules.logger import get_logger
+logger = get_logger(__name__)
+
 def main():
-    print("Hello from samp!")
+    file_path = Choose_file()
+    if not file_path:
+        logger.warning("Files Not Selected. please select a file.")
+        return
+    
+    output_folder = Choose_output_folder()
+    if not output_folder:
+        logger.warning("Output folder not selected. Please select an output folder.")
+        return
+    
+    logger.info(f"Selected File:{file_path}")
+    logger.info(f"Choosen Output Folder: {output_folder}")
 
+    process_excel_file(file_path, output_folder)
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "main":
+        main()
+        logger.info("Processing completed successfully.")
